eafb8ae1e7d6207431640436f97430d8f64adfeb
Richard Bown <rich@xyglo.com>
2023-12-03 19:03:30 +0100

    fixed but not fixed

--- 3/3.py
+++ 3/3.py
@@ -8,6 +8,8 @@ partMap = []
 specialCharMap = []
 numberMap = []
 
+partTwo = False
+
 for line in open(args.filename, 'r', encoding="utf-8"):
     line = line.strip()
     partMap.append(line)
@@ -21,8 +23,12 @@ for y in range(len(partMap)):
         #print(f"Line {y}")
         if not partMap[y][x].isnumeric():
             if partMap[y][x] != ".":
-                print(f"Found {partMap[y][x]} at {x},{y}")
-                specialCharMap.append((x,y))
+                if partTwo and partMap[y][x] == "*":
+                    print(f"Gear found {partMap[y][x]} at {x},{y}")
+                    specialCharMap.append((x,y))
+                else:
+                    print(f"Found {partMap[y][x]} at {x},{y}")
+                    specialCharMap.append((x,y))
 
             if startNumberPos != -1:
                 insertNumber = ""
@@ -34,28 +40,62 @@ for y in range(len(partMap)):
         else:
             if startNumberPos == -1:
                 startNumberPos = x
-            
+
 
 total: int = 0
 # locate all number blocks
 
-for pN in numberMap:
-    print(f"pN = {pN}")
-    
-    pX = int(pN[0])
-    pY = int(pN[1])
-    pValue = int(pN[2])
+if not partTwo:
+    for pN in numberMap:
+        print(f"pN = {pN}")
+        
+        pX = int(pN[0])
+        pY = int(pN[1])
+        pValue = int(pN[2])
+        previousTotal = total
 
-    # is there a special symbol within one
+        # is there a special symbol within one
+        for sC in specialCharMap:
+            sX = int(sC[0])
+            sY = int(sC[1])
+
+            #print(f"sC = {sC}")
+
+            # within 1 y
+            if sY >= pY - 1 and sY <= pY + 1 and sX >= pX - 1 and sX <= pX + len(pN[2]) :
+                total += pValue
+                print(f"{pValue} is a part")
+                break
+
+        if previousTotal == total:
+            print(f" --> EXCLUDED {pN}")
+else:
     for sC in specialCharMap:
         sX = int(sC[0])
         sY = int(sC[1])
 
-        print(f"sC = {sC}")
+        found1 = -1
+        found2 = -1
 
-        # within 1 y
-        if sY >= pY - 1 and sY <= pY + 1 and sX >= pX -1 and sX <= pX + len(pN[2]):
-            total +=  pValue
+        # now find two numbers near it if you can
+        #
+        for pN in numberMap:
+            print(f"pN = {pN}")
+
+            pX = int(pN[0])
+            pY = int(pN[1])
+            pValue = int(pN[2])
+            previousTotal = total
+            
+            if sY >= pY - 1 and sY <= pY + 1 and sX >= pX - 1 and sX <= pX + len(pN[2]):
+                if found1 == -1:
+                    found1 = pValue
+                else:
+                    found2 = pValue
+                    break
+
+        if found1 > 0 and found2 > 0:
+            total += found1 * found2
 
 
 print(f"Total = {total}")
